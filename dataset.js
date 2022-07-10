e06387f1d7ed764683600d3209f38443c0a2eb89
dcyanez <96788960+dcyanez@users.noreply.github.com>
2022-07-10 16:34:22 -0500

    Add files via upload

---
+++ dataset.js
@@ -0,0 +1,34 @@
+[
+   {
+      "x": 1000,
+      "grupo": "aguilas" 
+   },
+  {
+      "x": 750,
+      "grupo": "lobos" 
+   },
+   {
+      "x": 652,
+      "grupo": "castores" 
+   },
+   {
+      "x": 287,
+      "grupo": "tortugas" 
+   },
+   {
+      "x": 625 ,
+      "grupo": "panteras" 
+   },
+   {
+      "x": 985,
+      "grupo": "conejos" 
+   },
+   {
+      "x": 125,
+      "grupo": "serpientes" 
+   },
+   {
+      "x": 89,
+      "grupo": "lagartos" 
+   }
+]
